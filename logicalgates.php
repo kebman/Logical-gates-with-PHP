65c31259c30ea07540ca445114b2a1b2f63921b8
kebman <kebman@journalist.com>
2013-05-20 21:45:07 +0200

    Create logicalgates.php

---
+++ logicalgates.php
@@ -0,0 +1,53 @@
+<?php 
+class LogicalGates {
+  public and_gate($a, $b) {
+		if ($a and $b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public nand_gate($a, $b) {
+		if (!$a and !$b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public or_gate($a, $b) {
+		if ($a or $b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public nor_gate($a, $b) {
+		if (!$a or !$b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public xor_gate($a, $b) {
+		if ($a xor $b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public xnor_gate($a, $b) {
+		if (!$a xor !$b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public not_gate($a) {
+		if (!$a) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+?>
